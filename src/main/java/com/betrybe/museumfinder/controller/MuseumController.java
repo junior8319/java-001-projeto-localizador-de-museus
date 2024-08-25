a27d4a1188ae27ab227e9b6f53fcd798a2ff4a68
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2024-08-25 17:25:17 -0300

    feat-Req6-implements controller layer getClosestMuseum()

--- src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
+++ src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
@@ -2,13 +2,19 @@ package com.betrybe.museumfinder.controller;
 
 import com.betrybe.museumfinder.dto.MuseumCreationDto;
 import com.betrybe.museumfinder.dto.MuseumDto;
+import com.betrybe.museumfinder.model.Coordinate;
 import com.betrybe.museumfinder.model.Museum;
 import com.betrybe.museumfinder.service.MuseumServiceInterface;
+import com.betrybe.museumfinder.util.ModelDtoConverter;
+import org.springframework.core.annotation.AliasFor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -59,5 +65,28 @@ public class MuseumController {
     );
 
     return ResponseEntity.status(HttpStatus.CREATED).body(museumDto);
+  }
+
+  /**
+   * Gets closest museum.
+   *
+   * @param latitude    the latitude
+   * @param longitude   the longitude
+   * @param maxDistance the max distance
+   * @return the closest museum
+   */
+  @GetMapping("/closest")
+  public ResponseEntity<MuseumDto> getClosestMuseum(
+      @RequestParam(name = "lat") Double latitude,
+      @RequestParam(name = "lng") Double longitude,
+      @RequestParam(name = "max_dist_km") Double maxDistance
+  ) {
+    Coordinate coordinate = new Coordinate(latitude, longitude);
+
+    Museum museumFound = service.getClosestMuseum(coordinate, maxDistance);
+
+    MuseumDto museumDto = ModelDtoConverter.modelToDto(museumFound);
+
+    return ResponseEntity.status(HttpStatus.OK).body(museumDto);
   }
 }
